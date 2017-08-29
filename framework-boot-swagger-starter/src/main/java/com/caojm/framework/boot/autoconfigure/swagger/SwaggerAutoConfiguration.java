50349413e76076586336b48530d6b0306734fe11
jianmincao <a12345678>
2017-08-29 23:07:19 +0800

    swagger-starter

--- framework-boot-swagger-starter/src/main/java/com/caojm/framework/boot/autoconfigure/swagger/SwaggerAutoConfiguration.java
+++ framework-boot-swagger-starter/src/main/java/com/caojm/framework/boot/autoconfigure/swagger/SwaggerAutoConfiguration.java
@@ -2,9 +2,11 @@ package com.caojm.framework.boot.autoconfigure.swagger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -26,24 +28,14 @@ import java.time.LocalDateTime;
  */
 @Configuration
 @EnableSwagger2
-@ConditionalOnClass(EnableSwagger2.class)
+@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerAutoConfiguration {
     private final static Logger logger = LoggerFactory.getLogger(SwaggerAutoConfiguration.class);
 
-    /**
-     * 从哪个包开始扫描
-     */
-    @Value("${swagger.basePackage}")
-    private String SWAGGER_SCAN_BASE_PACKAGE;
-    /**
-     * API版本
-     */
-    @Value("${swagger.version:1.0-SNAPSHOT}")
-    private String VERSION ;
-    @Value("${swagger.title:Rest API 接口}")
-    private String title ;
-    @Value("${swagger.description:Rest API 接口}")
-    private String description ;
+    private static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+    @Autowired
+    private SwaggerProperties swaggerProperties;
+
 
     /**
      * 构建ApiInfo对象，这些信息会在swagger ui中进行展示
@@ -51,11 +43,11 @@ public class SwaggerAutoConfiguration {
      */
     private ApiInfo apiInfo() {
         ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-        apiInfoBuilder.title(title);    //api标题
-        apiInfoBuilder.description(description);    //api描述
+        apiInfoBuilder.title(swaggerProperties.getTitle());    //api标题
+        apiInfoBuilder.description(swaggerProperties.getDescription());    //api描述
         apiInfoBuilder.license("Terms of service");
         apiInfoBuilder.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0");
-        apiInfoBuilder.version(VERSION);    //版本号
+        apiInfoBuilder.version(swaggerProperties.getVersion());    //版本号
         apiInfoBuilder.contact(new Contact("Cao J.M.", "", "910922164@qq.com")); //
         return apiInfoBuilder.build();
     }
@@ -69,10 +61,10 @@ public class SwaggerAutoConfiguration {
     public Docket customImplementation() {
         logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         logger.info("开始自定义restful api swagger ui内容");
-        logger.info("SWAGGER_SCAN_BASE_PACKAGE={},title={},description={}",SWAGGER_SCAN_BASE_PACKAGE,title,description);
+        logger.info("SWAGGER_SCAN_BASE_PACKAGE={},title={},description={}",swaggerProperties.getBasePackage(),swaggerProperties.getTitle(),swaggerProperties.getDescription());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
                 .build()
                 .directModelSubstitute(LocalDate.class,  java.sql.Date.class)
                 .directModelSubstitute(LocalDateTime.class, java.util.Date.class)
---
+++ framework-boot-swagger-starter/src/main/java/com/caojm/framework/boot/autoconfigure/swagger/SwaggerProperties.java
@@ -0,0 +1,47 @@
+package com.caojm.framework.boot.autoconfigure.swagger;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+    private String version="1.0.0";
+    private String basePackage="com.caojm";
+    private String title="Rest API";
+    private String description="Rest API";
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getBasePackage() {
+        return basePackage;
+    }
+
+    public void setBasePackage(String basePackage) {
+        this.basePackage = basePackage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
